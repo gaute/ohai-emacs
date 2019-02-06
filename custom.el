f574259bfc430e6d55e8afd99253b656fda04166
Gaute Hvoslef Kvalnes <gaute@verdsveven.com>
2019-02-06 10:22:33 +0100

    Store my own customizations as well

---
+++ custom.el
@@ -0,0 +1,26 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" default)))
+ '(ohai-personal-taste/paredit t)
+ '(ohai-personal-taste/run-wizard nil)
+ '(ohai-personal-taste/splash nil)
+ '(ohai-personal-taste/style (quote dark))
+ '(ohai-personal-taste/training-wheels nil)
+ '(ohai-personal-taste/window-state (quote normal))
+ '(ohai/modules
+   (quote
+    (ohai-appearance ohai-fonts ohai-general ohai-splash ohai-ido ohai-navigation ohai-editing ohai-complete ohai-snippets ohai-codestyle ohai-dired ohai-project ohai-flycheck ohai-git ohai-orgmode ohai-help ohai-elisp ohai-helm ohai-eshell ohai-unicode ohai-refactor ohai-emoji ohai-smart-mode-line ohai-ext-window-nav ohai-lsp ohai-html ohai-markdown ohai-javascript ohai-clojure)))
+ '(package-selected-packages
+   (quote
+    (clojure-mode flow-mode tern js2-refactor js2-mode json-mode rainbow-mode web-mode lsp-ui company-lsp lsp-mode smart-mode-line rich-minority emojify emr unicode-fonts fish-completion bash-completion esh-autosuggest eshell-git-prompt helm-ext helm-flx helm-projectile helm-flycheck swiper-helm helm eros highlight-parentheses paredit discover-my-major which-key org-cliplink org-bullets org-plus-contrib git-gutter-fringe gist magit flycheck-color-mode-line flycheck ibuffer-projectile projectile ethan-wspace yasnippet company-try-hard company-emoji company-quickhelp company volatile-highlights shrink-whitespace expand-region multiple-cursors anzu ace-window avy flx-ido ido-vertical-mode smex ido-completing-read+ web material-theme diminish nyan-mode hlinum exec-path-from-shell f use-package paradox))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
