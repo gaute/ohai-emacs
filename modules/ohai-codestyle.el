c05f6aeb9a02ef2c76161d9900b952931c5a28aa
Gaute Hvoslef Kvalnes <gaute@verdsveven.com>
2019-02-20 09:43:11 +0100

    Disable ethan-wspace
    I couldn't figure out the conflict with editorconfig (?), which was
    nagging me with warnings.

--- modules/ohai-codestyle.el
+++ modules/ohai-codestyle.el
@@ -36,13 +36,13 @@
 ;; and automatically clean up your code when saving.
 ;; Use C-c c to instantly clean up your file.
 ;; Read more about ethan-wspace: https://github.com/glasserc/ethan-wspace
-(use-package ethan-wspace
-  :demand t
-  :commands global-ethan-wspace-mode
-  :config
-  (global-ethan-wspace-mode 1)
-  :bind ("C-c c" . ethan-wspace-clean-all)
-  :diminish ethan-wspace-mode)
+;; (use-package ethan-wspace
+;;   :demand t
+;;   :commands global-ethan-wspace-mode
+;;   :config
+;;   (global-ethan-wspace-mode 1)
+;;   :bind ("C-c c" . ethan-wspace-clean-all)
+;;   :diminish ethan-wspace-mode)
 
 (setq mode-require-final-newline nil)
 (setq require-final-newline nil)
